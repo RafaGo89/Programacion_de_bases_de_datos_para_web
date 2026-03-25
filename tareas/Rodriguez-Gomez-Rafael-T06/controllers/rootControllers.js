e9cfb5786ac3fe096b1a852d2257240ea0c6dc5b
Rafa Gómez <rafael.rodriguez7962@alumnos.udg.mx>
2026-03-24 22:02:37 -0600

    Realicé un sitio web multipágina con plantillas EJS y una plantilla Start Boostrap. Cuenta con la raiz, el contacto y el acerca de

---
+++ tareas/Rodriguez-Gomez-Rafael-T06/controllers/rootControllers.js
@@ -0,0 +1,11 @@
+exports.getRoot = (req, res, next) => {
+  res.render("index");
+};
+
+exports.getAbout = (req, res, next) => {
+  res.render("about");
+};
+
+exports.getContact = (req, res, next) => {
+  res.render("contact");
+};
